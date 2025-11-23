7eea4b40f92050dd202649d1494901bf48a1e557
saisudhanv <saisudhanv2004@gmail.com>
2025-11-23 12:36:24 +0530

    Changed

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+// import { assets } from '../assets/assets'
 import { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 
